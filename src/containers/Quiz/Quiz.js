464e62f1ce05f50f6dbe11e5c91326741a2ed1d1
Denis Berezin <nocab139@yandex.ru>
2020-04-30 19:10:01 +0300

    react quiz commit

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import './Quiz.css'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+class Quiz extends React.Component {
+
+    state = {
+
+        activeQuestion: 0,
+
+        answerState: null,
+
+        quiz: [
+            {
+                question: 'Какого цвета небо',
+                rightAnswerId: 2,
+                id: 1,
+                answers: [
+                    {text: 'Черный', id: 1},
+                    {text: 'Синий', id: 2},
+                    {text: 'Красный', id: 3},
+                    {text: 'зеленый', id: 4}
+                ]
+            },
+            {
+                question: 'В каком году основали Санкт-Петербург?',
+                rightAnswerId: 3,
+                id: 2,
+                answers: [
+                    {text: '1700', id: 1},
+                    {text: '1702', id: 2},
+                    {text: '1703', id: 3},
+                    {text: '1803', id: 4}
+                ]
+            }
+        ]
+    }
+
+    onAnswerClickHandler = (answerId) => {
+        console.log(answerId)
+
+        const question = this.state.quiz[this.state.activeQuestion]
+
+        if (question.rightAnswerId === answerId) {
+
+            this.setState({
+                answerState: {[answerId]: 'success'}
+
+            })
+
+            const timeout = window.setTimeout(() => {
+
+                if (this.isQuizFinished()) {
+                    console.log('finished')
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
+                    })
+                }
+
+                window.clearTimeout(timeout)  // Для того чтобы не было утечки памяти и таймаут убрался сразу после выполнения функции
+
+            }, 1000)
+
+
+        } else {
+
+            this.setState({
+                answerState: {[answerId]: 'error'}
+            })
+
+        }
+
+
+
+    }
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length
+    }
+
+    render() {
+        return (
+            <div className="quis">
+                <div className="quiz-wrapper">
+                    <h1>Ответьте на все вопросы</h1>
+                    <ActiveQuiz
+                        question={this.state.quiz[this.state.activeQuestion].question}
+                        answers={this.state.quiz[this.state.activeQuestion].answers}
+                        onAnswerClick={this.onAnswerClickHandler}
+                        quizLength={this.state.quiz.length}
+                        answerNumber={this.state.activeQuestion + 1}
+                        state={this.state.answerState}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Quiz
