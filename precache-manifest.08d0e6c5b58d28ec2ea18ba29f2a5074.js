186fb815040c8507bf1211402339aa2f8d9d7253
Denis Berezin <nocab139@yandex.ru>
2020-05-07 22:15:11 +0300

    Updates

---
+++ precache-manifest.08d0e6c5b58d28ec2ea18ba29f2a5074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e004db37df7c6d696b23031a08e06b",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "a542786fdc467c503b00",
+    "url": "/react-quiz/static/css/main.119b5e01.chunk.css"
+  },
+  {
+    "revision": "d106152faedae87026db",
+    "url": "/react-quiz/static/js/2.4654491f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz/static/js/2.4654491f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a542786fdc467c503b00",
+    "url": "/react-quiz/static/js/main.a187aba1.chunk.js"
+  },
+  {
+    "revision": "b1f2344ec01366e134c1",
+    "url": "/react-quiz/static/js/runtime-main.9e74c316.js"
+  }
+]);
